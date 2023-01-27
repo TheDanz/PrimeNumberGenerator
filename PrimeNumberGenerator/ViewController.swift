e40d2d11c6d328584d06856ae1be498670f69bf7
Danil Ryumin <ryumin_danil@icloud.com>
2023-01-27 16:01:19 +0300

    Create alerts in case of incorrect data

--- PrimeNumberGenerator/ViewController.swift
+++ PrimeNumberGenerator/ViewController.swift
@@ -13,8 +13,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func generateButtonClicked(_ sender: Any) {
-        guard let t = BInt(tTextField.text ?? "0") else { return }
-        guard let q = BInt(qTextField.text ?? "0") else { return }
+        guard let t = BInt(tTextField.text ?? "0") else {
+            presentAlert()
+            return
+        }
+        
+        guard let q = BInt(qTextField.text ?? "0") else {
+            presentAlert()
+            return
+        }
         
         let generator = Generator(t: t, q: q)
         generator.goto(.stepOne)
@@ -24,6 +31,13 @@ class ViewController: UIViewController {
     
     @IBAction func tap(_ sender: Any) {
         view.endEditing(true)
+    }
+    
+    func presentAlert() {
+        let alert = UIAlertController(title: "ERROR", message: "Enter correct data", preferredStyle: .alert)
+        let OKAlertAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(OKAlertAction)
+        present(alert, animated: true)
     }
 }
 
